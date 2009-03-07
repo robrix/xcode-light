fa1ca1c0fd7cc8674b8bb49c446855b405303cfb
Rob Rix <rix.rob@gmail.com>
2009-03-07 04:47:29 -0500

    Adding files to target will add them to a group named after their first parent subdirectory. That is, if you add a file View/Foo.h, it will be added to the View group, if one exists, or else to the default Classes group. This is handled for TM_SELECTED_FILES and TM_FILEPATH, so adding one or many files should work no matter where they reside in the project. If you don’t have either an appropriately-named group or a Classes group, then god help us all.

--- Support/bin/add_files_to_target.rb
+++ Support/bin/add_files_to_target.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+
 require "#{ENV['TM_SUPPORT_PATH']}/lib/tm/detach.rb"
 require "#{ENV['TM_SUPPORT_PATH']}/lib/ui.rb"
 require "#{ENV['TM_SUPPORT_PATH']}/lib/textmate.rb"
@@ -8,6 +9,24 @@ require "xcode"
 
 with Xcode::Project.select_project do |project|
 	TextMate.detach do
-		TextMate::UI.tool_tip project.groups["Classes"] << TextMate.selected_files
+		result = with(TextMate.selected_files || [ENV["TM_FILEPATH"]]) do |files|
+			if files
+				files_by_parent_folder = {}
+				files.collect do |file|
+					relative = file.sub(/^#{ENV["TM_PROJECT_DIRECTORY"]}\//, '')
+					group = relative[/(.+?)\//, 1]
+					files_by_parent_folder[group] = [] unless files_by_parent_folder[group]
+					files_by_parent_folder[group] << file
+				end
+				
+				files_by_parent_folder.inject("") do |memo, (group, grouped_files)|
+					memo << ((project.groups[group] || project.groups["Classes"]) << grouped_files)
+				end
+			else
+				"No files were selected, so nothing was added to the project."
+			end
+		end
+		
+		TextMate::UI.tool_tip(result)
 	end
 end
